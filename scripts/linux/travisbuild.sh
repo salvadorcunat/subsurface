3a114b652efd1f1de6f4c0fa9f51e461de6b8822
Salvador Cuñat <salvador.cunat@gmail.com>
2018-02-17 20:46:25 +0100

    smtk-travis libraries packaging 4

--- scripts/linux/travisbuild.sh
+++ scripts/linux/travisbuild.sh
@@ -64,7 +64,7 @@ cp -vf subsurface/smtk-import/smtk2ssrf.desktop smtk2ssrf_appdir/
 cp -vf install-root/bin/smtk2ssrf smtk2ssrf_appdir/usr/bin/
 cp -vf install-root/lib/libdivecomputer.so.0 smtk2ssrf_appdir/usr/lib/
 cp -vrf appdir/usr/plugins/ smtk2ssrf_appdir/usr/plugins/
-ldd smtk2ssrf_appdir/usr/bin/smtk2ssrf | grep "=>" |grep -v vdso | awk '{print $3}' | xargs -I '{}' cp -v '{}' smtk2ssrf_appdir/usr/lib
+ldd smtk2ssrf_appdir/usr/bin/smtk2ssrf | grep "=>" | grep -v not| grep -v vdso | awk '{print $3}' | xargs -I '{}' cp -v '{}' smtk2ssrf_appdir/usr/lib
 ldd smtk2ssrf_appdir/usr/lib/qt5/plugins/platforms/libqxcb.so | grep "=>" |grep -v not |grep -v vdso |grep -v ld-linux |awk '{print $3}' | xargs -I '{}' cp -v '{}' smtk2ssrf_appdir/usr/lib
 ./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -bundle-non-qt-libs -verbose=2
 ./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -appimage -verbose=2
