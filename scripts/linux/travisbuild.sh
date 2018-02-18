69e04a04b3f86446042af762331d997a46b99677
Salvador Cuñat <salvador.cunat@gmail.com>
2018-02-18 13:43:34 +0100

    smtk-travis despairing with linuxdeployqt 3
    Seems solved in local travis docker image tests

--- scripts/linux/travisbuild.sh
+++ scripts/linux/travisbuild.sh
@@ -49,7 +49,7 @@ export VERSION=$(cd ${TRAVIS_BUILD_DIR}/scripts ; ./get-version linux) # linuxde
 ./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -appimage -qmldir=./subsurface/map-widget/ -verbose=2
 find ./appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 
-#bash -e -x ./subsurface/scripts/smtk2ssrf-build.sh
+bash -e -x ./subsurface/scripts/smtk2ssrf-build.sh
 #unset QTDIR
 #unset QT_PLUGIN_PATH
 #unset LD_LIBRARY_PATH
@@ -58,21 +58,21 @@ find ./appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " "
 #find ./install-root
 ############################################
 
-#mkdir -pv ./smtk2ssrf_appdir/usr/share/metainfo
-#mkdir -pv ./smtk2ssrf_appdir/icons/hicolor/256x256/apps
-#mkdir -pv ./smtk2ssrf_appdir/usr/plugins
-#mkdir -pv ./smtk2ssrf_appdir/usr/bin
-#mkdir -pv ./smtk2ssrf_appdir/usr/lib/qt5/plugins
-#cp -vf subsurface/icons/subsurface-icon.svg smtk2ssrf_appdir/
-#cp -vf subsurface/smtk-import/smtk2ssrf.desktop smtk2ssrf_appdir/
-#cp -vf install-root/bin/smtk2ssrf smtk2ssrf_appdir/usr/bin/
-##cp -vf install-root/lib/libdivecomputer.so.0 smtk2ssrf_appdir/usr/lib/
-##cp -vf install-root/lib/libgit2* smtk2ssrf_appdir/usr/lib/
-##cp -vf install-root/lib/grantlee* smtk2ssrf_appdir/usr/lib
-##cp -vrf appdir/usr/plugins/ smtk2ssrf_appdir/usr/
-##ldd smtk2ssrf_appdir/usr/bin/smtk2ssrf | grep "=>" | grep -v not| grep -v vdso | awk '{print $3}' | xargs -I '{}' cp -v '{}' smtk2ssrf_appdir/usr/lib
-##ldd smtk2ssrf_appdir/usr/lib/qt5/plugins/platforms/libqxcb.so | grep "=>" |grep -v not |grep -v vdso |grep -v ld-linux |awk '{print $3}' | xargs -I '{}' cp -v '{}' smtk2ssrf_appdir/usr/lib
-#./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -bundle-non-qt-libs -verbose=2
-#./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -appimage -verbose=2
-#find ./smtk2ssrf_appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
+mkdir -pv ./smtk2ssrf_appdir/usr/share/metainfo
+mkdir -pv ./smtk2ssrf_appdir/icons/hicolor/256x256/apps
+mkdir -pv ./smtk2ssrf_appdir/usr/plugins
+mkdir -pv ./smtk2ssrf_appdir/usr/bin
+mkdir -pv ./smtk2ssrf_appdir/usr/lib/qt5/plugins
+cp -vf subsurface/icons/subsurface-icon.svg smtk2ssrf_appdir/
+cp -vf subsurface/smtk-import/smtk2ssrf.desktop smtk2ssrf_appdir/
+cp -vf install-root/bin/smtk2ssrf smtk2ssrf_appdir/usr/bin/
+cp -vf install-root/lib/libdivecomputer.so.0 smtk2ssrf_appdir/usr/lib/
+cp -vf install-root/lib/libgit2* smtk2ssrf_appdir/usr/lib/
+cp -vf install-root/lib/libGrantlee* smtk2ssrf_appdir/usr/lib/
+cp -vrf appdir/usr/plugins/ smtk2ssrf_appdir/usr/
+ldd smtk2ssrf_appdir/usr/bin/smtk2ssrf | grep "=>" |grep -v vdso | awk '{print $3}' | xargs -I '{}' cp -v '{}' smtk2ssrf_appdir/usr/lib
+ldd smtk2ssrf_appdir/usr/lib/qt5/plugins/platforms/libqxcb.so | grep "=>" |grep -v vdso |grep -v ld-linux |awk '{print $3}' | xargs -I '{}' cp -v '{}' smtk2ssrf_appdir/usr/lib
+./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -bundle-non-qt-libs -verbose=2
+./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -appimage -verbose=2
+find ./smtk2ssrf_appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 
