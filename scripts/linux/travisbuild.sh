2f33e6cd0b17521b4d1612ef5fc2a378dcd3f537
Salvador Cuñat <salvador.cunat@gmail.com>
2018-02-18 06:36:18 +0100

    smtk-travis packaging libraries 7

--- scripts/linux/travisbuild.sh
+++ scripts/linux/travisbuild.sh
@@ -64,6 +64,7 @@ cp -vf subsurface/smtk-import/smtk2ssrf.desktop smtk2ssrf_appdir/
 cp -vf install-root/bin/smtk2ssrf smtk2ssrf_appdir/usr/bin/
 cp -vf install-root/lib/libdivecomputer.so.0 smtk2ssrf_appdir/usr/lib/
 cp -vf install-root/lib/libgit2* smtk2ssrf_appdir/usr/lib/
+cp -vf install-root/lib/grantlee* smtk2ssrf_appdir/usr/lib
 cp -vrf appdir/usr/plugins/ smtk2ssrf_appdir/usr/
 #ldd smtk2ssrf_appdir/usr/bin/smtk2ssrf | grep "=>" | grep -v not| grep -v vdso | awk '{print $3}' | xargs -I '{}' cp -v '{}' smtk2ssrf_appdir/usr/lib
 #ldd smtk2ssrf_appdir/usr/lib/qt5/plugins/platforms/libqxcb.so | grep "=>" |grep -v not |grep -v vdso |grep -v ld-linux |awk '{print $3}' | xargs -I '{}' cp -v '{}' smtk2ssrf_appdir/usr/lib
