eaf97216eb5887927b7696b485cc7a2c9cd2b0a4
Salvador Cuñat <salvador.cunat@gmail.com>
2018-02-18 08:06:03 +0100

    smtk-travis despairing with linuxqtdeploy

--- scripts/linux/smtk2ssrf-travisbuild.sh
+++ scripts/linux/smtk2ssrf-travisbuild.sh
@@ -12,6 +12,30 @@ export CMAKE_PREFIX_PATH=$QT_ROOT/lib/cmake
 # the global build script expects to be called from the directory ABOVE subsurface
 cd ..
 
-bash -e -x ./subsurface/scripts/build.sh -desktop
 bash -e -x ./subsurface/scripts/smtk2ssrf-build.sh -b Debug
+
+export QT_PLUGIN_PATH=$QT_ROOT/plugins
+export QT_QPA_PLATFORM_PLUGIN_PATH=$QT_ROOT/plugins
+export QT_DEBUG_PLUGINS=1
+
+# for debugging: find $QT_ROOT/plugins
+
+env CTEST_OUTPUT_ON_FAILURE=1 make -C subsurface/build check
+
+mkdir -pv ./smtk2ssrf_appdir/usr/share/metainfo
+mkdir -pv ./smtk2ssrf_appdir/icons/hicolor/256x256/apps
+mkdir -pv ./smtk2ssrf_appdir/usr/plugins
+mkdir -pv ./smtk2ssrf_appdir/usr/bin
+mkdir -pv ./smtk2ssrf_appdir/usr/lib/qt5/plugins
+cp -vf subsurface/icons/subsurface-icon.svg smtk2ssrf_appdir/
+cp -vf subsurface/smtk-import/smtk2ssrf.desktop smtk2ssrf_appdir/
+cp -vf install-root/bin/smtk2ssrf smtk2ssrf_appdir/usr/bin/
+cp -rvf appdir/usr/plugins/* smtk2ssrf_appdir/usr/plugins/
+unset QTDIR
+unset QT_PLUGIN_PATH
+unset LD_LIBRARY_PATH
+
+./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -bundle-non-qt-libs -verbose=2
+./linuxdeployqt*.AppImage ./smtk2ssrf_appdir/smtk2ssrf.desktop -appimage -verbose=2
+find ./smtk2ssrf_appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 
