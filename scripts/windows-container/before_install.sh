d16e68c562dc3960009f3ba112c1b0d57445e048
Salvador Cuñat <salvador.cunat@gmail.com>
2018-10-12 18:42:27 +0200

    Testing yet another tarball in travis

--- scripts/windows-container/before_install.sh
+++ scripts/windows-container/before_install.sh
@@ -52,5 +52,5 @@ docker exec -t builder bash subsurface/scripts/get-dep-lib.sh single . mdbtools
 
 # get prebuilt static mxe libraries for glib.
 # do not overwrite upstream prebuilt mxe binaries if there is any coincidence.
-docker exec -t builder wget -q https://www.dropbox.com/s/fesfbzqzkgee5ut/mxe-static-minimal-97c0fbfd_1.tar.xz
-docker exec -t builder tar -C /win/mxe -xJf mxe-static-minimal-97c0fbfd_1.tar.xz --skip-old-files
+docker exec -t builder wget -q https://www.dropbox.com/s/8clyxo8zjx1arvn/mxe-static-minimal-97c0fbfd_2.tar.xz
+docker exec -t builder tar -C /win/mxe -xJf mxe-static-minimal-97c0fbfd_2.tar.xz --skip-old-files
